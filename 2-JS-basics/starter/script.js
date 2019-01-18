eb79dce98a81d57e8eb3c19c3154524250b3d8e1
Matt Smith <mattsmithereens@gmail.com>
2019-01-18 15:07:43 -0800

    excercise 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,14 +1,42 @@
-let johnHeight = 1.78;
-let johnWeight = 82;
-let markHeight = 1.84;
-let markWeight = 90;
+// quiz #1
 
-let johnBMI = johnWeight / (Math.pow(johnHeight, 2));
-let markBMI = markWeight / Math.pow(markHeight, 2);
+// let johnHeight = 1.78;
+// let johnWeight = 82;
+// let markHeight = 1.84;
+// let markWeight = 90;
 
-let johnBiggerThanMark = johnBMI > markBMI;
+// let johnBMI = johnWeight / (Math.pow(johnHeight, 2));
+// let markBMI = markWeight / Math.pow(markHeight, 2);
 
-console.log("John's BMI is " + johnBMI);
-console.log("Mark's BMI is " + markBMI);
+// let johnBiggerThanMark = johnBMI > markBMI;
 
-console.log("is John's BMI higher than Mark's? " + johnBiggerThanMark);
+// console.log("John's BMI is " + johnBMI);
+// console.log("Mark's BMI is " + markBMI);
+
+// console.log("is John's BMI higher than Mark's? " + johnBiggerThanMark);
+
+// quiz #2
+
+let johnScores = [89, 120, 103];
+let mikeScores = [116, 94, 123];
+let maryScores = [94, 134, 105];
+
+function getSum(total, num) {
+  return total + num;
+}
+
+let johnAvg = (johnScores.reduce(getSum)) / 3;
+let mikeAvg = (mikeScores.reduce(getSum)) / 3;
+let maryAvg = (maryScores.reduce(getSum)) / 3;
+
+//console.log('John = ' + johnAvg + "Mike = " + mikeAvg + 'Mary = ' + maryAvg);
+
+if (johnAvg > mikeAvg && johnAvg > maryAvg) {
+  console.log('John is the winner with a score of ' + johnAvg);
+} else if (mikeAvg > johnAvg && mikeAvg > maryAvg) {
+  console.log('Mike is the winner with a score of ' + mikeAvg);
+} else if (maryAvg > johnAvg && maryAvg > mikeAvg) {
+  console.log('Mary is the winner with a score of ' + maryAvg);
+} else {
+  console.log('there appears to be a tie.  The scores are: John: ' + johnAvg + ', Mike: ' + mikeAvg + ', Mary ' + maryAvg);
+}
