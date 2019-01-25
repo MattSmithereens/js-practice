f4ea4397ea86e8fbeb007440f9b8bed13ae327a8
Matt Smith <mattsmithereens@gmail.com>
2019-01-25 14:59:35 -0800

    coding challenge #3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -41,24 +41,86 @@
 //   console.log('there appears to be a tie.  The scores are: John: ' + johnAvg + ', Mike: ' + mikeAvg + ', Mary ' + maryAvg);
 // }
 
-function calculateAge(birthYear) {
-  return 2019 - birthYear;
-}
+// Function lesson
+
+// function calculateAge(birthYear) {
+//   return 2019 - birthYear;
+// }
 
 // let ageMatt = calculateAge(1975);
 // let ageCoco = calculateAge(1977);
 // console.log(ageMatt + ", " + ageCoco);
 
-function yearsUntilRetirement(year, firstName) {
-  let age = calculateAge(year);
-  let retirement = 65 - age;
-  if (retirement > 0) {
-    console.log(firstName + ' retires in ' + retirement + ' years.');
-  } else {
-    console.log('Why is ' + firstName + ' still working?');
-  }
+// function yearsUntilRetirement(year, firstName) {
+//   let age = calculateAge(year);
+//   let retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(firstName + ' retires in ' + retirement + ' years.');
+//   } else {
+//     console.log('Why is ' + firstName + ' still working?');
+//   }
 
+// }
+
+// yearsUntilRetirement(1975, 'Matt');
+// yearsUntilRetirement(1945, 'Pete');
+
+// Function Statements and Expressions
+
+// function declaration:
+// function WhatDoYouDo(job, firstName) { 
+
+// function expression
+// let WhatDoYouDo = function (job, firstName) {
+//   if (job === 'teacher') {
+//     return firstName + ' is a teacher';
+//   } else if (job === 'bike mechanic') {
+//     return firstName + ' is underpaid to fix shitbox bikes';
+//   } else {
+//     return firstName + ' does something else';
+//   }
+// }
+
+// console.log(WhatDoYouDo('bike mechanic', 'Matt'));
+// console.log(WhatDoYouDo('teacher', 'Dick'));
+// console.log(WhatDoYouDo('fluffer', 'Harry'));
+
+// arrays
+
+//initialize new array
+// let names = ['Tom', 'Dick', 'Harry'];
+// let years = new Array(1990, 1969, 1948);
+
+// Coding Challenge #3
+
+let calculateTip = function (bill) {          //initialize function and pass bill argument//
+  let percentage;                             //initialize percentage variable
+  if (bill > 0 && bill <= 50) {
+    percentage = .2;
+  } else if (bill > 50 && bill < 200) {
+    percentage = .15;
+  } else if (bill >= 200) {
+    percentage = .1;
+  } else {
+    alert('nope');
+  }
+  return bill * percentage;
 }
 
-yearsUntilRetirement(1975, 'Matt');
-yearsUntilRetirement(1945, 'Pete');
+let bills = [124, 48, 268];                   //initialize bill array
+let tips = [
+  calculateTip(bills[0]),
+  calculateTip(bills[1]),
+  calculateTip(bills[2])];
+let totalBill = [
+  bills[0] + calculateTip(bills[0]),
+  bills[1] + calculateTip(bills[1]),
+  bills[2] + calculateTip(bills[2])];
+
+console.log(tips);
+console.log(totalBill);
+
+
+
+// console.log(calculate(124));
+// console.log(calculate(268));
