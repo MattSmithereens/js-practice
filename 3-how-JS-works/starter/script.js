257728e04769e7158e0a645f0e7083a657604679
Matt Smith <mattsmithereens@gmail.com>
2019-02-05 22:50:35 -0800

    scoping lectures

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,20 +1,8 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+// Hoisting only works for function declarations
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// calculateAge(1965);
 
 
 ///////////////////////////////////////
@@ -23,21 +11,19 @@
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
 
 
 // Example to show the differece between execution stack and scope chain
@@ -67,11 +53,27 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+    console.log(this);
+}
 
+calculateAge(1965);
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1975,
+    calculateAge: function () {
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
 
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
 
-
-
-
+john.calculateAge();
