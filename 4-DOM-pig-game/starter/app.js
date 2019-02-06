968f65849c5d521549873d4bea7dbb4780075e05
Matt Smith <mattsmithereens@gmail.com>
2019-02-05 23:30:42 -0800

    manipulate dom

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
+
+document.querySelector('#current-0').textContent = dice;
