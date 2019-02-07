61446ac6cd6b95d7080da66df039897afe10270d
Matt Smith <mattsmithereens@gmail.com>
2019-02-07 15:11:23 -0800

    add init function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,26 +15,66 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-// initially do not render dice image
-document.querySelector('.dice').style.display = 'none';
+init();
 
-// initially set scores to zero according to id
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+function swap() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // ternary operator; ie very simple if then statement
+  roundScore = 0;
+  document.getElementById('current-0').textContent = "0";
+  document.getElementById('current-1').textContent = "0";
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+}
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   let dice = Math.floor(Math.random() * 6) + 1;
   let diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
-})
 
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    swap();
+  }
+});
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+  if (scores[activePlayer] > 10) {
+    document.querySelector('#name-' + activePlayer).textContent = 'WINNER!!!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    swap();
+  }
+});
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
 // selects "current-" object in document and passes value as text, ie textContent.  textContent cannot pass HTML
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
